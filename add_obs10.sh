3d3b4f7e5c210e3b8e6380d6ec492768cdc1e1de
Matthew Craig <mattwcraig@gmail.com>
2013-11-12 04:12:08 -0600

    Shell scripts for carrying out addition of observer to files

---
+++ add_obs10.sh
@@ -0,0 +1,15 @@
+python add_observer_to_data.py /Volumes/feder_data_originals/processed/2013-05-06 observer_table.csv
+python add_observer_to_data.py /Volumes/feder_data_originals/processed/2013-05-09 observer_table.csv
+python add_observer_to_data.py /Volumes/feder_data_originals/processed/2013-05-23 observer_table.csv
+python add_observer_to_data.py /Volumes/feder_data_originals/processed/2013-07-03 observer_table.csv
+python add_observer_to_data.py /Volumes/feder_data_originals/processed/2013-08-11 observer_table.csv
+python add_observer_to_data.py /Volumes/feder_data_originals/processed/2013-08-12 observer_table.csv
+python add_observer_to_data.py /Volumes/feder_data_originals/processed/2013-08-13 observer_table.csv
+python add_observer_to_data.py /Volumes/feder_data_originals/processed/2013-08-16 observer_table.csv
+python add_observer_to_data.py /Volumes/feder_data_originals/processed/2013-08-19 observer_table.csv
+python add_observer_to_data.py /Volumes/feder_data_originals/processed/2013-09-11 observer_table.csv
+python add_observer_to_data.py /Volumes/feder_data_originals/processed/2013-09-12 observer_table.csv
+
+python add_observer_to_data.py /Volumes/feder_data_originals/processed/2013-09-13 observer_table.csv
+python add_observer_to_data.py /Volumes/feder_data_originals/processed/2013-09-16 observer_table.csv
+python add_observer_to_data.py /Volumes/feder_data_originals/processed/2013-10-15 observer_table.csv
