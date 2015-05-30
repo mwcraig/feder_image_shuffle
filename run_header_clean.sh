f39175676e0659f5535eea1389f75de28c1529a0
Matthew Craig <mattwcraig@gmail.com>
2015-05-29 22:55:54 -0500

    Add a driver for the one-off scripts

---
+++ run_header_clean.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+dirs=$(find /Volumes/Dark_Matter/feder_processed/staged/201* -type d)
+
+echo "Copying scripts to..."
+for d in dirs
+do
+    echo "    $d"
+    cp 1-whack_image_and_update_overscan.sh delete_imagehw $d
+done
+
+echo "Running scripts"
+for d in dirs
+do
+    echo "    Starting $d..."
+    bash ./1-whack_image_and_update_overscan.sh
+    echo "    Finished $d"
+done
