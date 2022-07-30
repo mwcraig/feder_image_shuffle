74c0ef4b6e2603f6c5a66efd20db28aa11b342f8
Matt Craig <mattwcraig@gmail.com>
2022-07-30 10:56:35 -0500

    Add script from cron to automatically stage up to 5 nights

---
+++ auto_stage.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# STILL NEEDS ACCESS TO GITHUB TOKEN!!
+
+# Script to stage each night as they are taken. It is best to run this mid-day
+# i.e. noon-ish to ensure all syncing is done before staging.
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate astro
+conda info
+
+cd ~/feder_image_shuffle
+
+# stage the most recent night only
+
+now=$(date -I)
+
+export PATH="/usr/local/astrometry/bin:$PATH"
+echo PATH is $PATH
+echo solve-field at $(which solve-field) 
+
+bash stage_night.sh 7 &> run_staging_$now.txt
+
