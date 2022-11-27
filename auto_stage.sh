8a42e7f18a31dd4340427e95df3c1d63c32f083a
Matt Craig <mattwcraig@gmail.com>
2022-11-27 11:15:01 -0600

    Update to newer python environment

--- auto_stage.sh
+++ auto_stage.sh
@@ -6,7 +6,7 @@
 # i.e. noon-ish to ensure all syncing is done before staging.
 
 source ~/miniconda3/etc/profile.d/conda.sh
-conda activate astro
+conda activate astro310
 conda info
 
 cd ~/feder_image_shuffle
@@ -17,7 +17,7 @@ now=$(date -I)
 
 export PATH="/usr/local/astrometry/bin:$PATH"
 echo PATH is $PATH
-echo solve-field at $(which solve-field) 
+echo solve-field at $(which solve-field)
 
 bash stage_night.sh 7 &> run_staging_$now.txt
 
