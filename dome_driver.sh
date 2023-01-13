4223179ab1dc48ff6541c07a267542c66c00fc39
Matt Craig <mattwcraig@gmail.com>
2023-01-13 17:07:30 -0600

    Add driver script for dome check

---
+++ dome_driver.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# STILL NEEDS ACCESS TO GITHUB TOKEN!!
+
+# Script to stage each night as they are taken. It is best to run this mid-day
+# i.e. noon-ish to ensure all syncing is done before staging.
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate astro310
+conda info
+
+cd ~/feder_image_shuffle
+
+python check_dome.py
